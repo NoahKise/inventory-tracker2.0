432ac8c337b72893dc9b135aa7d29001e438cc8b
noahkise <noah@kisefamily.com>
2023-12-08 09:28:35 -0800

    add placeholder header

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+  return (
+    <React.Fragment>
+      <h1>Noah's Magic The Gathering Store</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi laoreet felis orci. Integer aliquet congue nibh, vitae viverra urna volutpat vitae. Phasellus tincidunt nisi sed venenatis placerat. Fusce lorem nulla, mollis vitae lorem sed, ultrices varius orci. Donec sodales lorem a nulla convallis porttitor. Praesent ut dapibus tortor. Duis vel semper dui, quis venenatis odio. Integer non augue ac elit fermentum lacinia nec nec lorem. Nullam lorem tellus, pharetra non faucibus a, rhoncus at nunc.</p>
+    </React.Fragment>
+  );
+}
+
+export default Header;
