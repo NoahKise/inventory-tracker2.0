9c096476b1d6e3d3cd48e7fe40f5540f9af09515
noahkise <noah@kisefamily.com>
2023-12-15 14:29:10 -0800

    add color change on stock remaining for low stock

--- src/components/StockItem.js
+++ src/components/StockItem.js
@@ -7,11 +7,15 @@ function StockItem({ name, price, releaseDate, imgSrc, leftInStock, description,
     update(id);
   };
 
+  const stockStyle = {
+    color: leftInStock < 10 ? 'red' : 'inherit',
+  };
+
   return (
     <div id="stockItem">
       <img src={imgSrc} alt={name} />
       <p><strong>{name} - {price}</strong></p>
-      <p>Left in Stock: {leftInStock}</p>
+      <p style={stockStyle}>Left in Stock: {leftInStock}</p>
       <p><em>Release Date: {releaseDate}</em></p>
       <p>{description}</p>
       <button onClick={handleSell}>Sell One Unit</button>
