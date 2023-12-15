3e69f221fd2ee2a35e9eb5264c84eb16e8bfb66e
noahkise <noah@kisefamily.com>
2023-12-15 11:25:32 -0800

    write and pass tests for boolean state slices

---
+++ src/__tests__/reducers/newItemFormVOPSlice.test.js
@@ -0,0 +1,12 @@
+import newItemFormVOPSliceReducer, { setNewItemFormVOPTrue, setNewItemFormVOPFalse } from './../../redux/newItemFormVOPSlice';
+
+describe('newItemFormVOPSliceReducer', () => {
+    it('should handle setNewItemFormVOPTrue action', () => {
+        const newState = newItemFormVOPSliceReducer(false, setNewItemFormVOPTrue());
+        expect (newState).toEqual(true);
+    });
+    it('should handle setNewItemFormVOPFalse action', () => {
+        const newState = newItemFormVOPSliceReducer(true, setNewItemFormVOPFalse());
+        expect (newState).toEqual(false);
+    });
+});
