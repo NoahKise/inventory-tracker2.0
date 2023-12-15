1cb2aba89ebdfc9f0887cf07c20cc3df6d26b952
noahkise <noah@kisefamily.com>
2023-12-15 09:19:20 -0800

    add store

---
+++ src/redux/store.js
@@ -0,0 +1,14 @@
+import { configureStore } from "@reduxjs/toolkit";
+import newItemFormVOPSliceReducer from "./newItemFormVOPSlice";
+import itemDetailVOPSliceReducer from "./itemDetailVOPSlice";
+import selectedIdSliceReducer from "./selectedIdSlice";
+import mainInventoryListSliceReducer from "./mainInventoryListSlice"; 
+
+export const store = configureStore({
+  reducer: {
+    newItemFormVOP: newItemFormVOPSliceReducer,
+    itemDetailVOP: itemDetailVOPSliceReducer,
+    selectedId: selectedIdSliceReducer,
+    mainInventoryList: mainInventoryListSliceReducer,
+  }
+});
